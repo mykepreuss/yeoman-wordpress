cb913c0e58c7daf981c16d07c267ee86c8682af2
Myke Preuss <myke@signals.ca>
2014-01-06 17:14:12 -0800

    fixing some workflow issues and adding browser_sync file for include

---
+++ app/templates/global/README.md
@@ -0,0 +1,2 @@
+# <%= themeName %> project
+---
---
+++ app/templates/global/browser_sync.php
@@ -0,0 +1,2 @@
+<script src='http://XXXXXXX:3000/socket.io/socket.io.js'></script>
+<script src='http://XXXXXXX:3001/browser-sync-client.min.js'></script>
